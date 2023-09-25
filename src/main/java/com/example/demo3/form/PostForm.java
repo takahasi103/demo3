56073d31047784a12a88bdb74e0fdb6b1d650fca
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-25 22:06:28 +0900

    PostForm完成

--- src/main/java/com/example/demo3/form/PostForm.java
+++ src/main/java/com/example/demo3/form/PostForm.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-public class PostsForm {
+public class PostForm {
 	// titleへのバリデーション設定を追加
 	@Size(min = 1, max = 200)
 	private String title;
